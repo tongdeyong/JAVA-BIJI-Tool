a748439600761df4af0f40e9b3ea952c35f1ba6b
童德勇 <41535256+tongdeyong@users.noreply.github.com>
2019-03-27 13:28:10 +0800

    删除不必要文字

--- src/main/java/com/deyong/javalearning/service/impl/ContentService.java
+++ src/main/java/com/deyong/javalearning/service/impl/ContentService.java
@@ -1,12 +1,3 @@
-/*
- * Copyright(c): 2018 中国民航信息网络股份有限公司 All rights reserved.
- * 密级：CONFIDENTIAL
- * 项目名称：新一代航空公司业务前端
- * 模块名称：ancillary-service
- * 文件名称：FlightPO
- * 创建日期：2019-03-14
- * 注意：本内容仅限于中国民航信息网络股份有限公司内部传阅，禁止外泄以及用于其他的商业目的.
- */
 package com.deyong.javalearning.service.impl;
 
 import com.deyong.javalearning.controller.model.Content;
