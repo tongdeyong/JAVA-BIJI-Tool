a56243c111e851502078d122090bbf664faf611f
tongdeyong <313718582@qq.com>
2019-04-02 03:04:40 +0800

    修复序列号从0开始的bug

--- src/main/resources/static/js/util/HandlebarsUtil.js
+++ src/main/resources/static/js/util/HandlebarsUtil.js
@@ -1,11 +1,18 @@
 define(function (require, exports, module) {
 
+    require('../dist/handlebars-v4.1.0.js');
+
     var templates = [];
 
     //寻找模板
     var compileTemplate = function (template) {
         var $templateScript = $('#' + template);
         var type = $templateScript.attr('type');
+        //注册索引+1的helper
+        Handlebars.registerHelper("addOne", function (index) {
+            //返回+1之后的结果
+            return index + 1;
+        });
         if (type === 'text/x-handlebars-template') {
             return Handlebars.compile($templateScript.html());
         }
