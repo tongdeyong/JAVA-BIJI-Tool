ca2893851bc105c6eb7321a66509976b1bcc5f6f
童德勇 <41535256+tongdeyong@users.noreply.github.com>
2019-04-03 11:21:06 +0800

    修复查看详情时，分类不出选项的bug

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -2,6 +2,8 @@ define(function (require, exports, module) {
 
     require('./util/jquery.base64.js');
     var $ = jQuery;
+    var AjaxUtil = require('./util/AjaxUtil.js');
+    var HandlebarsUtil = require('./util/HandlebarsUtil.js');
 
     //当提交按钮显示为编辑时，走编辑
     var editData = function () {
@@ -20,20 +22,25 @@ define(function (require, exports, module) {
     };
 
     /*显示当前选项*/
-    var chooseOptionToSHow = function (contentClass) {
-        $('#contentClass option').each(function () {
-            console.log($(this).val());
-            if ($(this).val() === contentClass) {
-                $(this).attr('selected', true);
-            }
-        })
+    var chooseOptionToShow = function (contentClass) {
+        var url = './content/queryClass';
+        AjaxUtil.ajaxGet(url, null, function (data) {
+            data = $.grep(data, function (val, index) {
+                return val !== contentClass;
+            });
+            var html = HandlebarsUtil.render('choose-template', data);
+            $('#contentClass').append(html);
+        }, function () {
+            alert('分类查询异常！')
+        });
     };
 
     /*显示详细信息*/
-    var detailToShow = function (getDetailData) {
+    function detailToShow(getDetailData) {
         //插入数据
         //分类
-        chooseOptionToSHow(getDetailData.contentClass);
+        $('#contentClass').append('<option>' + getDetailData.contentClass + '</option>');
+        chooseOptionToShow(getDetailData.contentClass);
         //大标题
         $('#parentTitle').val(getDetailData.parentTitle).attr("disabled", "disabled");
         //小标题
@@ -44,7 +51,7 @@ define(function (require, exports, module) {
         $('#add-class-button').hide();
         //id放入
         $('#idValue').val(getDetailData.id);
-    };
+    }
 
     var dataToShow = function () {
         var url = decodeURI(window.location.href);
@@ -52,7 +59,7 @@ define(function (require, exports, module) {
         var detailData = argIndex[1].split('=');
         var content = JSON.parse($.base64.decode(detailData[1], 'uft8'));
         detailToShow(content);
-        if (detailData[0] ==='edit'){
+        if (detailData[0] === 'edit') {
             editData();
         }
 
